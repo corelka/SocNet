1c895a4c1d6fda3e4708090f2597fb0f88a35bbf
Максим Калин <max-man12@mail.ru>
2016-02-03 13:32:57 +0200

    Test

--- src/SocNet/Startup.cs
+++ src/SocNet/Startup.cs
@@ -50,6 +50,7 @@ namespace SocNet
             services.AddMvc();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
